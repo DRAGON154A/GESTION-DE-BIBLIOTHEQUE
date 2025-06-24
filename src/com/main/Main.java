e01673f9cf132c6a40b9e14ff26ab7f8dce0637c
DRAGON154A <ateufoarthur@gmail.com>
2025-06-24 21:56:15 +0100

    mise à jour des interface de vue login et singup

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,5 +1,7 @@
 package com.main;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
@@ -7,18 +9,20 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) {
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root, 400, 300);
-        primaryStage.setTitle("Ma première application JavaFX");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+ @Override
+    public void start(Stage stage) throws IOException {
+        // Chargement du fichier FXML pour définir l'interface utilisateur
+        // Assurez-vous que "primary.fxml" est dans src/main/resources/com/exemple/monapp/
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 640, 480); // Création de la scène avec une taille par défaut
+        
+        stage.setTitle("Ma Première App JavaFX !"); // Titre de la fenêtre
+        stage.setScene(scene); // Définition de la scène pour la fenêtre
+        stage.show(); // Affichage de la fenêtre
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch(); // Méthode standard pour lancer une application JavaFX
     }
-
 
 }
