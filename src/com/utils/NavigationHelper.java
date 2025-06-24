c13031fb0ae3ad4f40853b97841b9f2361ce0b00
brayanno <teupebrayanno@gmail.com>
2025-06-24 18:03:05 +0100

    mise a jour

---
+++ src/com/utils/NavigationHelper.java
@@ -0,0 +1,29 @@
+package com.utils;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class NavigationHelper {
+
+	public void changerPage(Node eventSource,String cheminFileFxml,String titre)
+	{
+		try
+		{
+			FXMLLoader loader = new FXMLLoader(NavigationHelper.class.getResource(cheminFileFxml));
+			Parent root = loader.load();
+			
+			Stage stage = (Stage) eventSource.getScene().getWindow();
+			stage.setTitle(titre);
+			stage.setScene(new Scene(root));
+			stage.show();
+		}catch(IOException e) {
+			System.err.println("Erreur lors du changement de la page");
+			e.printStackTrace();
+		}
+	}
+}
