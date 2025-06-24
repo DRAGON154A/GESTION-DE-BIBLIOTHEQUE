30ed638e18135222dea976ad3dae027025f29844
DRAGON154A <ateufoarthur@gmail.com>
2025-06-24 13:16:11 +0100

    adding things

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,10 +1,24 @@
 package com.main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Main extends Application {
 
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 400, 300);
+        primaryStage.setTitle("Ma première application JavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 
 }
